75b6013c706207a017a883bb77a801c3f8ff76b2
maddalena <mdn.luca@gmail.com>
2015-01-14 09:18:47 +0100

    prova

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")    # SQLite
 SQLALCHEMY_DATABASE_URI = "mysql://testuser:test@localhost/alchemytest"
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")            # migrate data file
+
+# ---------------------
